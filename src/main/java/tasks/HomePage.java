bde6c97d890445fcc65ef2fec40f03c0cb8f9848
JeimyA <jeimy.arguellor@sqasa.co>
2024-05-21 18:30:11 -0500

    commit 2 para el reto

---
+++ src/main/java/tasks/HomePage.java
@@ -0,0 +1,18 @@
+package tasks;
+
+
+import com.sun.jmx.snmp.tasks.Task;
+
+public class HomePage implements Task {
+
+
+    @Override
+    public void cancel() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
